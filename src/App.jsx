4b2576802a353f4a8816d67eef8a6cc46af990ac
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-25 11:55:47 -0400

    removed css to start fresh

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import General from './components/General';
 import CVSection from './components/Section';
 
 function App() {
-  <CVSection>
-    <General text="Why is this not working??" />
-  </CVSection>;
+  return (
+    <CVSection>
+      <General text="Why is this not working??" />
+    </CVSection>
+  );
 }
 
 export default App;
