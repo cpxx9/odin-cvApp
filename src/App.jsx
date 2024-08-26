590c3e6c8c6aeac3ac8ccea7a3ff0e2a9f4fc5f8
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-26 16:30:34 -0400

    commented out starter data

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,44 @@ import CVDisplay from './components/CVDisplay';
 
 function App() {
   const [personalDetails, setPersonalDetails] = useState({
-    fullName: 'John Doe',
-    currentPosition: 'System Administrator',
-    phoneNumber: '8881114444',
-    email: 'john@doe.com',
-    location: 'Palo Alto, US'
+    // fullName: 'John Doe',
+    // currentPosition: 'System Administrator',
+    // phoneNumber: '8881114444',
+    // email: 'john@doe.com',
+    // location: 'Palo Alto, US'
   });
 
   const [educationDetails, setEducationDetails] = useState([
-    {
-      school: 'MIT',
-      degree: 'Computer Science',
-      startDate: '2018-09',
-      endDate: '2022-05',
-      location: 'Boston, USA',
-      id: uuid()
-    },
-    {
-      school: 'Yale',
-      degree: 'Data Engineering',
-      startDate: '2019-10',
-      endDate: '2023-06',
-      location: 'New Haven, USA',
-      id: uuid(),
-      displayId: uuid()
-    }
+    // {
+    //   school: 'MIT',
+    //   degree: 'Computer Science',
+    //   startDate: '2018-09',
+    //   endDate: '2022-05',
+    //   location: 'Boston, USA',
+    //   id: uuid()
+    // },
+    // {
+    //   school: 'Yale',
+    //   degree: 'Data Engineering',
+    //   startDate: '2019-10',
+    //   endDate: '2023-06',
+    //   location: 'New Haven, USA',
+    //   id: uuid(),
+    //   displayId: uuid()
+    // }
   ]);
 
   const [workDetails, setWorkDetails] = useState([
-    {
-      company: 'Test Company',
-      position: 'Engineer',
-      startDate: '2019-12',
-      endDate: 'present',
-      location: 'Palo Alto, USA',
-      description: '',
-      id: uuid()
-    }
+    // {
+    //   company: 'Test Company',
+    //   position: 'Engineer',
+    //   startDate: '2019-12',
+    //   endDate: 'present',
+    //   location: 'Palo Alto, USA',
+    //   description: '',
+    //   id: uuid(),
+    //   displayId: uuid()
+    // }
   ]);
 
   function addWorkSection() {
@@ -55,7 +56,8 @@ function App() {
       endDate: '',
       location: '',
       description: '',
-      id: uuid()
+      id: uuid(),
+      displayId: uuid()
     };
     let newArr = [...workDetails, newSection];
     setWorkDetails(newArr);
