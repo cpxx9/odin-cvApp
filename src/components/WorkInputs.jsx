b1c98d4436354bf46410a29e91fee567d85682a9
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-08-28 11:09:26 -0400

    show and hide for work inputs now working

--- src/components/WorkInputs.jsx
+++ src/components/WorkInputs.jsx
@@ -1,4 +1,5 @@
 import CustomInput from './CustomInput';
+import { useState } from 'react';
 
 export default function WorkInputs({ workDetails, setWorkDetails, currentId }) {
   const currentIndex = workDetails.findIndex((e) => e.id === currentId);
@@ -45,72 +46,84 @@ export default function WorkInputs({ workDetails, setWorkDetails, currentId }) {
     setWorkDetails(newArr);
   }
 
+  const [panelActive, setPanelActive] = useState(true);
+
+  function toggleShow() {
+    const newActive = !panelActive;
+    setPanelActive(newActive);
+  }
+
   return (
     <div className="input-btn-section">
-      <ul className="inputs-list">
-        <li>
-          <label>
-            Company:
-            <CustomInput
-              placeholder={'Company name'}
-              value={workDetails[currentIndex].company}
-              onChange={handleCompanyChange}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            Position held:
-            <CustomInput
-              placeholder={'Position held'}
-              value={workDetails[currentIndex].position}
-              onChange={handlePositionChange}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            Start Date:
-            <input
-              type="month"
-              placeholder="xxxx-xx"
-              value={workDetails[currentIndex].startDate}
-              onChange={handleStartChange}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            End Date:
-            <input
-              type="month"
-              placeholder="xxxx-xx"
-              value={workDetails[currentIndex].endDate}
-              onChange={handleEndChange}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            Location:
-            <CustomInput
-              placeholder={'Location'}
-              value={workDetails[currentIndex].location}
-              onChange={handleLocationChange}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            Description:
-            <textarea
-              placeholder="Short description about what you did here"
-              value={workDetails[currentIndex].description}
-              onChange={handleDescriptionChange}
-            />
-          </label>
-        </li>
-      </ul>
+      <button onClick={toggleShow}>{panelActive ? 'hide' : 'show'}</button>
+      {panelActive ? (
+        <ul className="inputs-list">
+          <li>
+            <label>
+              Company:
+              <CustomInput
+                placeholder={'Company name'}
+                value={workDetails[currentIndex].company}
+                onChange={handleCompanyChange}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Position held:
+              <CustomInput
+                placeholder={'Position held'}
+                value={workDetails[currentIndex].position}
+                onChange={handlePositionChange}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Start Date:
+              <input
+                type="month"
+                placeholder="xxxx-xx"
+                value={workDetails[currentIndex].startDate}
+                onChange={handleStartChange}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              End Date:
+              <input
+                type="month"
+                placeholder="xxxx-xx"
+                value={workDetails[currentIndex].endDate}
+                onChange={handleEndChange}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Location:
+              <CustomInput
+                placeholder={'Location'}
+                value={workDetails[currentIndex].location}
+                onChange={handleLocationChange}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              Description:
+              <textarea
+                placeholder="Short description about what you did here"
+                value={workDetails[currentIndex].description}
+                onChange={handleDescriptionChange}
+              />
+            </label>
+          </li>
+        </ul>
+      ) : (
+        workDetails[currentIndex].company
+      )}
       <button onClick={removeWorkItem}>Remove</button>
     </div>
   );
